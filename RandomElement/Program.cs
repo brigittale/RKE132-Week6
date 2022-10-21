ed20b47385be6a45f6e7c7e0668a10eee07c8a53
Brigitta Lees <brigitta.lees@gmail.com>
2022-10-21 14:36:00 +0300

    Add week 6 code

---
+++ RandomElement/Program.cs
@@ -0,0 +1,18 @@
+﻿//string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "takeaway" };
+
+//Random rnd = new Random();
+
+//int randomIndex = rnd.Next(0, snacks.Length);
+
+//Console.WriteLine($"Tonight we are going to eat {snacks[randomIndex]}.");
+
+string hello = "Hello, world!".();
+int counter = 0;
+for (int i = 0; i < hello.Length; i++)
+{
+    if counter([i] == 'l')
+    {
+        hello++;
+    }
+}
+Console.WriteLine($"There are {counter} l's in the string \"{hello}\"");
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿string hello = " we are the champions?";
+
+int stringLength = hello.Length;
+
+//string trimmedString = hello.Trim();
+
+hello = hello.Trim();
+
+char firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+hello = hello.Replace(hello[0], char.ToUpper(hello[0])).Replace('?', '!');
+
+firstLetter = hello[0];
+Console.WriteLine(firstLetter);
+
+Console.WriteLine(hello);
+
+
+
+//int wordCounter = 1;
+
+//for(int i = 0; i < hello.Length; i++)
+//{
+//    if (char.IsWhiteSpace(hello[i]))
+//    {
+//        wordCounter++;
+//    }
+//}
+
+//Console.WriteLine($"Word count in {hello}: {wordCounter} words.");
